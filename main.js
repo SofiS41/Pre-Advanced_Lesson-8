e113a9618dbe1544b8324958c7f20e63b28e5b76
SofiS41 <78041431+SofiS41@users.noreply.github.com>
2022-06-20 15:13:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 // ------- TASK 1 ---------
-var city;
+let city;
 city = 'Київ';
 city = 'Львів';
-var address = city;
+let address = city;
 console.log('-----TASK 1-----\n', address);
 // ------- TASK 2 -------
-// let num: number = Number(prompt('Введіть число'));
-// let message: string = num%2 == 0 ? "Число парне" : num%2 > 0 ? "Число непарне" : "Число 0";
-// console.log('\n-----TASK 2-----\n', message);
+let num = Number(prompt('Введіть число'));
+let message = num % 2 == 0 ? "Число парне" : num % 2 > 0 ? "Число непарне" : "Число 0";
+console.log('\n-----TASK 2-----\n', message);
 // ------- TASK 3 ------
-function maxNum() {
-    var arg = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        arg[_i] = arguments[_i];
-    }
-    return Math.max.apply(Math, arg);
+function maxNum(...arg) {
+    return Math.max(...arg);
 }
 console.log('\n-----TASK 3-----\n', maxNum(1, 8, 14, 3, 0, -3, 22, 5));
 // ------- TASK 4 ------
 function getSqrt(number) {
     if (typeof number == 'number' && number > 0)
-        return "\u041A\u0432\u0430\u0434\u0440\u0430\u0442\u043D\u0438\u0439 \u043A\u043E\u0440\u0456\u043D\u044C \u0437 ".concat(number, " \u0434\u043E\u0440\u0456\u0432\u043D\u044E\u0454 ").concat(Math.sqrt(number));
+        return `Квадратний корінь з ${number} дорівнює ${Math.sqrt(number)}`;
     else if (typeof number !== 'number' && typeof number !== 'undefined')
         return 'Повинно бути числове значення';
     else if (typeof number == 'number' && number <= 0)
@@ -33,5 +29,32 @@ console.log('2 - відємне число - ', getSqrt(-10));
 console.log('3 - нічого не ввели - ', getSqrt());
 console.log('4 - ввели число - ', getSqrt(25));
 // ------- TASK 5 -------
-var form = document.forms['badWords__form'];
-console.log('\n-----TASK 5-----\n', form);
+const form = document.forms['badWords__form'], formCheck = document.forms['badWords__form-check'], wordsList = document.querySelector('.bad-words__list_list');
+form.add.addEventListener('click', (e) => {
+    e.preventDefault();
+    let wordMas = wordsList.textContent.split(', ');
+    let newWord = form.word.value;
+    if (!wordMas.includes(newWord)) {
+        wordsList.textContent += ', ' + newWord;
+    }
+    else {
+        form.word.value = '';
+        form.word.placeholder = 'Таке слово вже є в списку';
+    }
+});
+form.resett.addEventListener('click', (e) => {
+    e.preventDefault();
+    wordsList.textContent = '';
+});
+formCheck.check.addEventListener('click', (e) => {
+    e.preventDefault();
+    let wordMas = wordsList.textContent.split(', ');
+    let text = formCheck.text.value;
+    console.log(wordMas);
+    wordMas.forEach(el => {
+        let substitute = '*';
+        text = text.replace(el, substitute.repeat(el.length));
+    });
+    formCheck.text.value = text;
+});
+console.log('\n-----TASK 5-----\n', 'Watch result in textarea');
